e6cfcdb4894e50d3cf20b11f51ef1f200f7d7683
alvarezyhana <manuelsonyasis45@gmail.com>
2025-03-22 22:38:50 +0800

    Create prefix.js

---
+++ script/prefix.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const configPath = "./config.json";
+
+// Load bot configuration
+const config = JSON.parse(fs.readFileSync(configPath));
+
+module.exports = {
+    name: "prefix",
+    usePrefix: false, // No need to require a prefix for this command
+    usage: "prefix",
+    version: "1.0",
+    execute(api, event, args) {
+        const botPrefix = config.prefix || "/";
+        const botName = config.botName || "My Bot";
+
+        let message = ` *Bot Information* \n`;
+        message += `⚙ *Prefix:* ${botPrefix}\n`;
+        message += `👑� *Bot Name:* ${botName}`;
+
+        api.sendMessage(message, event.threadID, event.messageID);
+    }
+};
